aa04df141240888b74201d28f245b327982d5e65
vaeruiz <vaeruiz@gmail.com>
2021-02-22 14:24:03 +0100

    Ejercicio 2 subido

---
+++ introduccion/ejercicio2.php
@@ -0,0 +1,12 @@
+<?php
+
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['SERVER_ADDR'];
+echo "<br>";
+echo $_SERVER['SERVER_SOFTWARE'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['REMOTE_ADDR'];
+?>
