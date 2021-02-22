4c0ac3709288271448e2b505a06043dd22aca7cc
vaeruiz <vaeruiz@gmail.com>
2021-02-22 18:18:37 +0100

    Ejercicio 2 resuelto

--- introduccion2/ejercicio1.php
+++ introduccion2/ejercicio1.php
@@ -1,6 +1,6 @@
 <?php
 
-$variable = rand (0, 1);
+$variable = rand (0,1);
 if ($variable == 1) {
     echo "<img src='imagenes/monedacara.jpg'>";
 } else {
---
+++ introduccion2/ejercicio2.php
@@ -0,0 +1,18 @@
+<?php
+
+$variable = rand (0,10);
+echo "$variable";
+if ($variable >= 0 and $variable < 5) {
+    echo "Insuficiente";
+} elseif ($variable >= 5 and $variable < 6) {
+    echo "Suficiente";
+} elseif ($variable >= 6 and $variable < 7) {
+    echo "Bien";
+}
+elseif ($variable >= 7 and $variable < 9) {
+    echo "Notable";
+}
+else {
+    echo "Sobresaliente";
+}
+?>
