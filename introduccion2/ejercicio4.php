dd5babbdd095270e783b82bddead155e3660c596
vaeruiz <vaeruiz@gmail.com>
2021-02-22 21:44:04 +0100

    Ejercicio 4 resuelto

--- introduccion2/ejercicio4.php
+++ introduccion2/ejercicio4.php
@@ -3,17 +3,17 @@
 $variable = rand (1,6);
 echo "Ha salido $variable";
 echo "<br>";
-if ($variable = 1) {
-    echo "<img src='introduccion2/imagenes/monedacara.jpg'>";
-} elseif ($variable = 2) {
-    echo "<img src='introduccion2/imagenes/dado2.png'>";
-} elseif ($variable = 3) {
-    echo "<img src='introduccion2/imagenes/dado3.png'>";
-} elseif ($variable = 4) {
-    echo "<img src='introduccion2/imagenes/dado4.png'>";
-} elseif ($variable = 5) {
-    echo "<img src='introduccion2/imagenes/dado5.png'>";
+if ($variable == 1) {
+    echo "<img src='imagenes/dado1.jpg'>";
+} elseif ($variable == 2) {
+    echo "<img src='imagenes/dado2.jpg'>";
+} elseif ($variable == 3) {
+    echo "<img src='imagenes/dado3.jpg'>";
+} elseif ($variable == 4) {
+    echo "<img src='imagenes/dado4.png'>";
+} elseif ($variable == 5) {
+    echo "<img src='imagenes/dado5.jpg'>";
 } else {
-    echo "<img src='introduccion2/imagenes/dado6.png'>";
+    echo "<img src='imagenes/dado6.jpg'>";
 }
 ?>
