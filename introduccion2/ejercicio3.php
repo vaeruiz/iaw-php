750ed4e5c42610946fa04858040c1791d312e17d
vaeruiz <vaeruiz@gmail.com>
2021-02-22 18:32:37 +0100

    Ejercicio 3 resuelto

---
+++ introduccion2/ejercicio3.php
@@ -0,0 +1,32 @@
+<?php
+
+$variable = rand (1,7);
+echo "Ha salido $variable";
+echo "<br>";
+switch ($variable){
+    case 1:
+        echo "Lunes";
+        break;
+    case 2:
+        echo "Martes";
+        break;
+    case 3:
+        echo "Miércoles";
+        break;
+    case 4:
+        echo "Jueves";
+        break;
+    case 5:
+        echo "Viernes";
+        break;
+    case 6:
+        echo "Sábado";
+        break;
+    case 7:
+        echo "Domingo";
+        break;
+
+
+   }
+
+?>
