c6a4583c0c6c8ccd4a8f4184186c23c878cebec4
vaeruiz <vaeruiz@gmail.com>
2021-02-22 17:32:48 +0100

    Ejercicio 3 terminado

--- introduccion/ejercicio3.php
+++ introduccion/ejercicio3.php
@@ -1,5 +1,56 @@
 <?php
+echo "<pre>";
+echo "\$GLOBALS";
+echo "<br>";
+print_r ($GLOBALS);
+echo "<br>";
 
+echo "<pre>";
+echo "\$_SERVER";
+echo "<br>";
+print_r ($_SERVER);
+echo "<br>";
 
+echo "<pre>";
+echo "\$_GET";
+echo "<br>";
+print_r ($_GET);
+echo "<br>";
+
+echo "<pre>";
+echo "\$_POST";
+echo "<br>";
+print_r ($_POST);
+echo "<br>";
+
+echo "<pre>";
+echo "\$_FILES";
+echo "<br>";
+print_r ($_FILES);
+echo "<br>";
+
+echo "<pre>";
+echo "\$_COOKIE";
+echo "<br>";
+print_r ($_COOKIE);
+echo "<br>";
+
+echo "<pre>";
+echo "\$_SESSION";
+echo "<br>";
+print_r ($_SESSION);
+echo "<br>";
+
+echo "<pre>";
+echo "\$_REQUEST";
+echo "<br>";
+print_r ($_REQUEST);
+echo "<br>";
+
+echo "<pre>";
+echo "\$_ENV";
+echo "<br>";
+print_r ($_ENV);
+echo "<br>";
 
 ?>
