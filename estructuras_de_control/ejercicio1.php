3901cff2e0f34b30255197d2d07e2248f62e4bbb
vaeruiz <vaeruiz@gmail.com>
2021-02-23 13:57:28 +0100

    Actualizacion

--- estructuras_de_control/ejercicio1.php
+++ estructuras_de_control/ejercicio1.php
@@ -1,7 +1,7 @@
 <?php
 
 $variable = rand (0,1);
-if ($variable = 1) {
+if ($variable == 1) {
     echo "<img src='imagenes/monedacara.jpg'>";
 } else {
     echo "<img src='imagenes/monedacruz.jpg'>";
