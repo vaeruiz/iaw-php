98eeffe7445696e49496402a6bbacbf33f1c38fc
vaeruiz <vaeruiz@gmail.com>
2021-02-25 12:28:29 +0100

    Ejercicios acabados

---
+++ bucles/ejemplo.php
@@ -0,0 +1,19 @@
+<?php
+
+$i = 1;
+echo "<h1>Ascendente</h1>";
+echo "<br>";
+for ($i = 1; $i <= 10; $i++){
+    echo $i;
+    echo "<br>";
+}
+
+
+echo "<h1>Descendente</h1>";
+echo "<br>";
+for ($i = 10; $i >= 0; $i--){
+    echo $i;
+    echo "<br>";
+}
+
+?>
---
+++ bucles/ejemplo_while.php
@@ -0,0 +1,19 @@
+<?php
+$i = 1;
+while ($i <=10){
+    echo "<table>
+            <caption>$i</caption>
+            <caption>$i</caption>
+          </table>";
+    echo "<br>";
+    $i++;
+}
+
+$i = 10;
+while ($i >= 0){
+    echo $i;
+    echo "<br>";
+    $i--;
+}
+
+?>
