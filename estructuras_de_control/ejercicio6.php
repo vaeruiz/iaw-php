b0e29a39fa952f027674e5c68abd883c4c900f9c
vaeruiz <vaeruiz@gmail.com>
2021-02-25 10:54:26 +0100

    Ejercicios acabados

--- estructuras_de_control/ejercicio6.php
+++ estructuras_de_control/ejercicio6.php
@@ -2,5 +2,6 @@
 
 $variable = rand(1,6);
 echo "<h1>Ha salido $variable</h1>";
-echo "<img src='imagenes/dado$variable.jpg'>"
+echo "<img src='imagenes/dado$variable.jpg'>";
+
 ?>
--- estructuras_de_control/ejercicio7.php
+++ estructuras_de_control/ejercicio7.php
@@ -1,6 +1,9 @@
 <?php
 
-$dado1 = rand()
-
+$variable1 = rand(1,6);
+$variable2 = rand(1,6);
+echo "<h1>Ha salido $variable1 y $variable2</h1>";
+echo "<img src='imagenes/dado$variable1.jpg'>";
+echo "<img src='imagenes/dado$variable2.jpg'>";
 
 ?>
